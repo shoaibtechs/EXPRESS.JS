237654aa83a44aa6e56e08dc4ba0544a8b25d5fa
shoaibtechs <p200147@pwr.nu.edu.pk>
2025-10-28 18:17:44 +0500

    Add Handle request file

---
+++ 2_handling_request.js
@@ -0,0 +1,22 @@
+const express =  require("express");
+const app = express();
+
+let port = 8080;
+
+
+app.listen(port, ()=>{
+
+    console.log(`The server is listening at port ${port}`);
+
+
+})
+
+
+
+app.use((req, res)=>{
+
+    console.log("Request Received ");
+
+
+
+})
