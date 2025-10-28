993969b0cf7cfbbba1437a4a651934c95c47ddc4
shoaibtechs <p200147@pwr.nu.edu.pk>
2025-10-28 18:10:41 +0500

    refactor: rename index.js to server.js

---
+++ 1_server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+
+let port =  8080;
+
+
+app.listen(port, ()=>{
+
+    console.log(`The server is listening at port ${port}`);
+
+
+})
+
